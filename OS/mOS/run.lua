15b57eeaf2d1071183f7337c1b46509cf1bb8e82
HardProfi <38588716+HardProfi@users.noreply.github.com>
2018-04-21 22:19:36 +0300

    Update run.lua

--- OS/mOS/run.lua
+++ OS/mOS/run.lua
@@ -2,6 +2,9 @@ local fs = require("filesystem")
 local cp = require("computer")
 local string = require("string")
 local ep = require("component").eeprom
+local io = require("io")
+local shell = require("shell")
+local os = require("os")
 
 if fs.exists("/mOS/pass.sc") == false then
 local pass = string.reverse(cp.address())
@@ -9,4 +12,16 @@ local file = fs.open("/mOS/pass.sc","w")
 file:write(pass)
 file:close()
 ep.setData(pass)
+else
+local file = io.open("/mOS/pass.sc","r")
+local pass = file:read("a")
+
+
+if ep.getData() == pass then
+shell.execute("gui.lua")
+else
+print("Вы пытались запустить mOS не офицальном устройстве")
+os.sleep(2)
+shell.execute("shutdown")
+end
 end
