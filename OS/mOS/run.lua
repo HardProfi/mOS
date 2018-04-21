9fbdd353de926cca2b66ac751311acef8aa0dbec
HardProfi <38588716+HardProfi@users.noreply.github.com>
2018-04-21 21:33:57 +0300

    Update run.lua

--- OS/mOS/run.lua
+++ OS/mOS/run.lua
@@ -1,1 +1,12 @@
-print("Test")
+local fs = require("filesystem")
+local cp = require("computer")
+local string = require("string")
+local ep = require("component").eeprom
+
+if fs.exists("/mOS/pass.sc") == false then
+local pass = string.reverse(cp.address())
+local file = fs.open("/mOS/pass.sc","w")
+file:write(pass)
+file:close()
+ep.setData(pass)
+end
