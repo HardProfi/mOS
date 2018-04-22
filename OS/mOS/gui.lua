2e5b127aa4c1136cee6da39992512d9176b21b37
HardProfi <38588716+HardProfi@users.noreply.github.com>
2018-04-22 07:13:14 +0300

    Update gui.lua

--- OS/mOS/gui.lua
+++ OS/mOS/gui.lua
@@ -1,5 +1,6 @@
 local cpm = require("component")
 local gpu = cpm.gpu
 local term = require("term")
+local Ws,Hs = gpu.getResolution()
 gpu.setBackground(0xa5a5a5)
-term.clear()
+gpu.fill(1,Hs-7,Ws,8," ")
