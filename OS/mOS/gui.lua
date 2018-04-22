e3dead46bb9b9900ff0060f1c6cb30aeb7ea8ae3
HardProfi <38588716+HardProfi@users.noreply.github.com>
2018-04-22 07:24:05 +0300

    Update gui.lua

--- OS/mOS/gui.lua
+++ OS/mOS/gui.lua
@@ -3,4 +3,18 @@ local gpu = cpm.gpu
 local term = require("term")
 local Ws,Hs = gpu.getResolution()
 gpu.setBackground(0xa5a5a5)
-gpu.fill(1,Hs-7,Ws,8," ")
+local page = 1
+local args = {...}
+
+if args[1] == "desktop" then 
+page = 2
+end
+
+
+
+
+while true do
+  if page == 1 then
+    print("e")
+  end
+end
